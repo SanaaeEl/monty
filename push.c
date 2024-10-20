23a0f6c2ca0cf04ccea374930ee78dfccbdcbbb1
SanaaeEl <assanaae@gmail.com>
2024-10-20 10:52:16 +0100

    initial

---
+++ push.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+ * _push - pushes an element to the stack
+ * @head: head of stack
+ * @l_count: line counter
+ */
+void _push(stack_t **head, unsigned int l_count)
+{
+}
