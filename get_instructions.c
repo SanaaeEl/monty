f86ecfcb16fe571d60a5e89ef7dc60973f2ca1db
SanaaeEl <assanaae@gmail.com>
2024-10-20 18:06:06 +0100

    init

--- get_instructions.c
+++ get_instructions.c
@@ -21,7 +21,7 @@ int get_instruction(char *line, stack_t *stack, unsigned int counter, FILE *file
 	for (i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++)
 	{
 		if (strcmp(instructions[i].opcode, opcode) == 0)
-			instructions[i].f(stack, counter, file);
+			instructions[i].f(stack, counter);
 	}
 
 	fprintf(stderr, "L%d: unknown instruction\n", counter);
