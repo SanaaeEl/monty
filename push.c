d09c1b2ebd47c1c555b15197b2cc2af66839dab5
SanaaeEl <assanaae@gmail.com>
2024-10-20 22:58:06 +0100

    push

--- push.c
+++ push.c
@@ -7,6 +7,27 @@
  */
 void _push(stack_t **head, unsigned int l_count)
 {
-	(void) head;
-	(void) l_count;
+	char *arg;
+	stack_t *new;
+
+	arg = strtok(NULL, " \n\t");
+	if (arg == NULL || !isinteger(arg))
+	{
+		fprintf(stderr, "L%d: USAGE: push integer\n", l_count);
+		exit(EXIT_FAILURE);
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		exit(EXIT_FAILURE);
+
+	new->n = atoi(arg);
+	new->next = *stack;
+	new->prev = NULL;
+
+	if (*stack != NULL)
+		(*stack)->prev = new;
+
+	*stack = new;
+
 }
