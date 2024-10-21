5ef753f7a43d95837321c70606507a3a3fa12f1b
SanaaeEl <assanaae@gmail.com>
2024-10-21 11:03:48 +0100

    error fix

--- push.c
+++ push.c
@@ -9,19 +9,31 @@ void _push(stack_t **head, unsigned int l_count)
 {
 	char *arg;
 	stack_t *new;
+	char *endptr;
+	long value;
 
 	arg = strtok(NULL, " \n\t");
-	if (arg == NULL || !isinteger(arg))
+	if (arg == NULL || *arg == '\0')
 	{
 		fprintf(stderr, "L%d: USAGE: push integer\n", l_count);
 		exit(EXIT_FAILURE);
 	}
 
+	value = strtol(arg, &endptr, 10);
+	if (*endptr != '\0' || endptr == arg)
+	{
+		fprintf(stderr, "L%d: USAGE push integer\n", l_count);
+		exit(EXIT_FAILURE);
+	}
+
 	new = malloc(sizeof(stack_t));
 	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
+	}
 
-	new->n = atoi(arg);
+	new->n = (int)value;
 	new->next = *stack;
 	new->prev = NULL;
 
